1d535eb8895b5e1343ed3a0af70b1e4e183f0c82
Larie Amimirog <amimirog@gmail.com>
2025-05-04 19:03:20 +0800

    Added colors for tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        royalpurple: "#5D3A6A",
+        royalgold: "#D4AF37",
+      },
+    },
   },
   plugins: [],
 }
